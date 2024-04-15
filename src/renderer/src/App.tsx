193647971cc3bce637afc2241369515a21cbd1a9
Steven Frady <sfrady20@gmail.com>
2024-04-15 14:06:48 -0400

    initial commit (tailwind not working)

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/renderer/components/ui/button";
+
+export default function () {
+  return (
+    <div className="container">
+      <Button>Yo</Button>
+    </div>
+  );
+}
